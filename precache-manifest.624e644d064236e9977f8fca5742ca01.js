698bad9537eecd14a65c42ff84817ce366a5093e
vicky <s353397@sinyi.com.tw>
2019-08-26 11:47:22 +0800

    Updates

---
+++ precache-manifest.624e644d064236e9977f8fca5742ca01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2dfcc428a361344bb8517b77562aad4",
+    "url": "/bookingHotel/index.html"
+  },
+  {
+    "revision": "a466111e503ff218874e",
+    "url": "/bookingHotel/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "365ae6e6de7d16df711f",
+    "url": "/bookingHotel/static/js/2.eab39a13.chunk.js"
+  },
+  {
+    "revision": "a466111e503ff218874e",
+    "url": "/bookingHotel/static/js/main.d9041ec8.chunk.js"
+  },
+  {
+    "revision": "28da1d20be8ab079f22a",
+    "url": "/bookingHotel/static/js/runtime~main.fac3d92c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bookingHotel/static/media/logo.5d5d9eef.svg"
+  }
+]);
